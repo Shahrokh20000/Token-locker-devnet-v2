a3521916f9b018b76d27598322ef0eda01c67b90
Elishaokon13 <eliscodavid@gmail.com>
2025-01-13 12:58:48 +0100

    fixed minor SEO issue

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,26 +9,27 @@ export default function Document() {
         <link rel="icon" href="https://cdn.prod.website-files.com/667c57e6f9254a4b6d914440/667d7104644c621965495f6e_LogoMark.svg" />
 
         {/* Primary Meta Tags */}
-        <meta name="title" content="VaultNAD" />
-        <meta name="description" content="A brief description of your website or project." />
-        <meta name="keywords" content="Your, Keywords, Here" />
-        <meta name="author" content="Your Name" />
-        <meta name="robots" content="index, follow" />
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Lock and manage your tokens securely on the blockchain with VaultNad. Protect your assets with transparency and trust." />
+        <meta name="keywords" content="VaultNad, token locker, blockchain, DeFi, secure token management, Web3, asset protection" />
+        <meta name="author" content="Monad DevRel Intern" />
+
+        {/* Title */}
+        <title>VaultNad - Secure Blockchain Token Locker</title>
 
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.VaultNad.xyz/" />
-        <meta property="og:title" content="VaultNad" />
-        <meta property="og:description" content="A brief description of your website or project." />
-        <meta property="og:image" content="https://www.VaultNad.xyz/logo.png" />
+        <meta property="og:url" content="https://vault-nad.vercel.app/" />
+        <meta property="og:title" content="VaultNad - Secure Blockchain Token Locker" />
+        <meta property="og:description" content="Lock and manage your tokens securely on the blockchain with VaultNad. Protect your assets with transparency and trust." />
+        <meta property="og:image" content="https://vault-nad.vercel.app//og-image.png" /> {/* You can add an image here */}
 
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://www.VaultNad.xyz/" />
-        <meta property="twitter:title" content="VaultNad" />
-        <meta property="twitter:description" content="A brief description of your website or project." />
-        <meta property="twitter:image" content="https://cdn.prod.website-files.com/667c57e6f9254a4b6d914440/667d7104644c621965495f6e_LogoMark.svg" />
+        <meta property="twitter:url" content="https://vault-nad.vercel.app/" />
+        <meta property="twitter:title" content="VaultNad - Secure Blockchain Token Locker" />
+        <meta property="twitter:description" content="Lock and manage your tokens securely on the blockchain with VaultNad. Protect your assets with transparency and trust." />
+        <meta property="twitter:image" content="https://vault-nad.vercel.app//og-image.png" /> {/* You can add an image here */}
 
         {/* Google Analytics */}
         <Script
